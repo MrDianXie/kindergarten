59e6cd05394da8ce735f228f47259e6eae7f349a
MrDianXie <2248568604@qq.com>
2023-01-14 02:11:07 +0800

    项目初始化

---
+++ kindergarten-admin-web/src/main/java/com/dq408/kindergarten/KindergartenApplication.java
@@ -0,0 +1,13 @@
+package com.dq408.kindergarten;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KindergartenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KindergartenApplication.class, args);
+    }
+
+}
---
+++ kindergarten-admin-web/src/test/java/com/dq408/kindergarten/KindergartenAdminWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dq408.kindergarten;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KindergartenAdminWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
