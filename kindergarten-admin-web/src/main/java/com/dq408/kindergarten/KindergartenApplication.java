7838d366ea7c5c0242a91e9e19ccbaa24b2de2de
MrDianXie <2248568604@qq.com>
2023-01-14 02:22:55 +0800

    项目初始化

--- kindergarten-admin-web/src/main/java/com/dq408/kindergarten/KindergartenApplication.java
+++ kindergarten-admin-web/src/main/java/com/dq408/kindergarten/KindergartenApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class KindergartenApplication {
 
     public static void main(String[] args) {
+        System.out.println("https://github.com/MrDianXie/kindergartens.git");
         SpringApplication.run(KindergartenApplication.class, args);
     }
 
